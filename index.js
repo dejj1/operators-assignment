8652e68420dee65f62bfdfb66625f3c16fa752ba
deji <dejioyelakin@gmail.com>
2023-02-20 21:51:22 +0100

    add 2 files

---
+++ index.js
@@ -0,0 +1,29 @@
+//SCIENCE SUBJECTS - Physics, Chemistry, Biology, Technical Drawing 
+//SOCIAL SCIENCE SUBJECTS - Accounting, Commerce, marketing, Geography
+//ARTS SUBJECTS - Government, Literature, History, Economics
+//GENERAL SUBJECTS - English, Mathematics
+let classGroup = 'ARTS'
+if (classGroup = 'ARTS') {
+    console.log('English, Mathematics, Government, Literature, History, Economics')
+} 
+else if (classGroup = 'SOCIAL SCIENCE') {
+    console.log('English, Mathematics, Accounting, Commerce, marketing, Geography')
+} 
+else if (classGroup = 'SCIENCE') {
+    console.log('English, Mathematics, Physics, Chemistry, Biology, Technical Drawing')
+} 
+else {
+    console.log('English, Mathematics')
+} 
+
+
+
+let num = 20;
+for(let index =2; index < 20; index = index**2){
+    pwr = index
+}
+console.log("The number " + pwr + " is the power of 2 nearest to " + num);
+
+    
+
+
---
+++ readme.md
@@ -0,0 +1,13 @@
+1. (i)Arithmetic Operators: +, -, /, *
+   (ii)Assignment Operators: =
+   (iii)Comparison Operators: >, <, >=, <=
+   (iv)Logical Operators: &&, ||, !
+   (v)Bitwise Operators: &, |, ^, <<, >>, ~
+
+2. (i)Arithmetic Operators: Addition and Subtraction
+   (ii)Assignment Operators: Basic Assignment(=), Addition Assignment(+=), Modulo(%=)
+   (iii)Comparison Operators: Less Than(<), Greater Than(>), Less Than or Equal to(<=), Greater Than or Equal To(>=)
+   (iv)Logical Operators: Logical AND, Logical OR, Logical NOT
+   (v)Bitwise Operators: Bitwise AND, Bitwise XOR
+
+3. 1, 8, 15 
